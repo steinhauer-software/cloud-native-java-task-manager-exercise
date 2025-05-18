677e399e4b37b99f6f93182e8e0a713505266e22
Holger Steinhauer <holger.steinhauer@steinhauer.software>
2025-05-18 11:55:14 +0200

    Initial commit
    Code, generated via IntelliJ's "New Project" and gitignore rules comming from
    
    ```
    gi intellij+all,macos,linux,windows,visualstudiocode,spring,gradle > .gitignore
    ```

---
+++ src/main/java/software/steinhauer/schulung/cloudnative/taskmanager/CloudNativeJavaTaskManagerExerciseApplication.java
@@ -0,0 +1,13 @@
+package software.steinhauer.schulung.cloudnative.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeJavaTaskManagerExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNativeJavaTaskManagerExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/software/steinhauer/schulung/cloudnative/taskmanager/CloudNativeJavaTaskManagerExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package software.steinhauer.schulung.cloudnative.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNativeJavaTaskManagerExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
